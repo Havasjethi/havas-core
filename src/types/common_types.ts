705a86c60f41ad532f46a5ce3f657e4078a953cf
Jozsef Kulcsar <havasjethi@gmail.com>
2021-10-25 21:53:36 +0200

    Save

--- src/types/common_types.ts
+++ src/types/common_types.ts
@@ -1,18 +1,30 @@
-
-export type HttpMethodType = 'all' | 'get' | 'post' | 'put' | 'delete' | 'patch' | 'options' | 'head';
+export type HttpMethodType =
+  | 'all'
+  | 'get'
+  | 'post'
+  | 'put'
+  | 'delete'
+  | 'patch'
+  | 'options'
+  | 'head';
 
 export interface DecoratedParameters {
   index?: number;
   name: string;
-  arguments?: any[];
+  arguments?: unknown[];
 }
+
+export type PostProcessor<Input = unknown, Outout = unknown> = (value: Input) => Outout;
 
 export interface RegistrableMethod {
   methodName: string;
   parameters: DecoratedParameters[];
+  postProcessors?: {
+    [parameterIndex: number]: PostProcessor[];
+  };
 }
 
-export interface CoreEndpoint extends RegistrableMethod{
+export interface CoreEndpoint extends RegistrableMethod {
   methodType: HttpMethodType;
   path: string;
 }
